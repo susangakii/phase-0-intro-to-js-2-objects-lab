df94e7e157e1f49be2a225f6d995e360ddb91cdc
Susan Gakii <susangakii71@gmail.com>
2025-08-28 11:45:33 +0300

    js objects

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+let employee = {
+    name: "Sam",
+    streetAddress: "123 NY St"
+};
+
+// Non-destructive update
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+    ...employee,
+    [key]: value
+  };
+}
+
+
+// Destructive update
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value;
+    return employee;
+}
+
+
+//Delete (Non-Destructive)
+function deleteFromEmployeeByKey(employee, name){
+    let newEmployee={...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+
+//Delete (Destructive)
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee.name;
+    return employee;
+}
